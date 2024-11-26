fdc44f3bfc537952ff206dac13ff3a98b374ad43
Danny Akulov <akulov.danny@gmail.com>
2024-11-27 00:04:48 +0300

    feat: init project configuration

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import * as process from "node:process";
+import {NestFactory} from "@nestjs/core";
+import {AppModule} from "./app.module";
+
+async function start(){
+    const PORT = process.env.PORT || 5000
+    const app = await NestFactory.create(AppModule)
+
+    await app.listen(PORT, () => console.log(`Listening on ${PORT}`))
+}
+
+start()
